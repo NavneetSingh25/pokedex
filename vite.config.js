42ddfc74be3d2d0c85fdfb42a9460883efda2e07
Navneet Singh <navneetsingh9234@gmail.com>
2025-07-08 23:59:04 +0530

    added changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base:'/pokedex/',
   build: {
     outDir: 'build', // This should specify the output directory
   },
